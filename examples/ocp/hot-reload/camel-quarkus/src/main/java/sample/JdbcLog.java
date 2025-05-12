9d7a389b0caae7b0deb1af35be76fc5994f99496
Claudio Miranda <claudio@claudius.com.br>
2025-05-12 19:21:35 +0100

    use quarkus datasource

--- examples/ocp/hot-reload/camel-quarkus/src/main/java/sample/JdbcLog.java
+++ examples/ocp/hot-reload/camel-quarkus/src/main/java/sample/JdbcLog.java
@@ -1,50 +1,66 @@
 package sample;
 
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 import javax.sql.DataSource;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import io.agroal.api.AgroalDataSource;
+import io.agroal.api.security.NamePrincipal;
+import io.agroal.api.security.SimplePassword;
+import jakarta.inject.Inject;
 import jakarta.inject.Provider;
 
 public class JdbcLog extends RouteBuilder {
 
+    // @Inject
+    // Logger LOG;
+/*
     @jakarta.inject.Inject
-    DataSource datasource(
+    public DataSource datasource(
         // @PropertyInject("{{secret:secret-basic-auth/username}}") String username,
         // @PropertyInject("{{secret:secret-basic-auth/password}}") String password,
         // @PropertyInject("{{secret:secret-basic-auth/host}}") String host,
         // @PropertyInject("{{secret:secret-basic-auth/port:5432}}") int port) {
         // quarkus way 1
-        // @ConfigProperty(name = "{{secret:secret-basic-auth/username}}") Provider<String> username,
-        // @ConfigProperty(name = "{{secret:secret-basic-auth/password}}") Provider<String> password,
-        // @ConfigProperty(name = "{{secret:secret-basic-auth/host}}") Provider<String> host,
-        // @ConfigProperty(name = "{{secret:secret-basic-auth/port:5432}}") Provider<Integer> port) {
+        @ConfigProperty(name = "{{secret:secret-basic-auth/username}}") Provider<String> username,
+        @ConfigProperty(name = "{{secret:secret-basic-auth/password}}") Provider<String> password,
+        @ConfigProperty(name = "{{secret:secret-basic-auth/host}}") Provider<String> host,
+        @ConfigProperty(name = "{{secret:secret-basic-auth/port:5432}}") Provider<Integer> port) {
         // quarkus way 2
-        @ConfigProperty(name = "${username}") Provider<String> username,
-        @ConfigProperty(name = "${password}") Provider<String> password,
-        @ConfigProperty(name = "${host}") Provider<String> host,
-        @ConfigProperty(name = "${port:5432}") Provider<Integer> port) {
+        // @ConfigProperty(name = "${username}") Provider<String> username,
+        // @ConfigProperty(name = "${password}") Provider<String> password,
+        // @ConfigProperty(name = "${host}") Provider<String> host,
+        // @ConfigProperty(name = "${port:5432}") Provider<Integer> port) {
         org.apache.commons.dbcp2.BasicDataSource ds = new org.apache.commons.dbcp2.BasicDataSource();
-        System.out.println(">>> creating datasource");
+        LOG.info(">>> teste");
+        LOG.info(">>> teste");
+        LOG.info(">>> teste");
+        LOG.info(">>> teste");
         ds.setUsername(username.get());
         ds.setPassword(password.get());
         ds.setUrl("jdbc:postgresql://" + host.get() + ":" + port.get() + "/testdb");
         ds.setDriverClassName("org.postgresql.Driver");
         return ds;
     }
-
+*/
     @Override
     public void configure() throws Exception {
-
         from("timer:JAVA?period=30s")
             .setBody(simple("SELECT * FROM foo"))
-            .to("jdbc:datasource")
+            .to("jdbc:my-camel")
             .process(e -> e.getContext().getRegistry().findByType(DataSource.class).forEach(b -> {
-                org.apache.commons.dbcp2.BasicDataSource ds = (BasicDataSource) b;
-                log.info(">> registry DataSource.getUserName(): " + ds.getUserName());
+                AgroalDataSource ds = (AgroalDataSource) b;
+                Principal princ = ds.getConfiguration().connectionPoolConfiguration().connectionFactoryConfiguration().principal();
+                log.info("> agroal user: " + princ.getName());
             }))
-            .log("resolving username: {{secret:secret-basic-auth/username}} - body: ${body}");
+            .log("resolving username from camel kubernetes function: {{secret:secret-basic-auth/username}}")
+            .log("body: ${body}");
     }
 }
